b18bc6b6e52eefd5bcefb10ab800800cceaac2be
Marcos Augusto Bitetti <marcosbitetti@gmail.com>
2009-06-01 22:28:26 -0300

    Ela ja esta falando e eu apanhando do ruby

--- bot.rb
+++ bot.rb
@@ -1,8 +1,17 @@
 require "time"
 
-require "twitter_sendeador.rb"
+#require File.expand_path(File.dirname(__FILE__)) + '/twitter_control'
+require File.expand_path(File.dirname(__FILE__)) + '/data/frases'
 
-puts "Cibele em acao"
+puts "           _                             "
+puts "         /' `\       /'                /'"
+puts "       /'   ._)    /'                /'"
+puts "     /'       O  /'__     ____     /' ____"
+puts "   /'       /' /'    )  /'    )  /' /'    )"
+puts " /'       /' /'    /' /(___,/' /' /(___,/'"
+puts "(_____,/'(__(___,/(__(________(__(________"
+puts "                                          "
+puts "                                em acao..."
 
 agora = Time::new
 puts agora.hour.to_s + ":" + agora.min.to_s + ":" + agora.sec.to_s
@@ -11,4 +20,6 @@ compare = Time::parse "19:32:55"
 
 puts compare>agora
 
+puts Frases::frase[ rand Frases::frase.length ]
 
+#TODO: Criar classe Memoria para ela alembrar dq falow das ultimas vezes!
